6f645ad32c4a1cc7952f265acb04e60abf1861ce
Noah Koo <sjdpclsrnrk@naver.com>
2023-04-20 22:21:31 +0900

    feat(detail): Add useEffect

--- src/routes/Detail.jsx
+++ src/routes/Detail.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 const Detail = (props) => {
@@ -6,9 +6,25 @@ const Detail = (props) => {
   let findId = props.shoes.find((arr) => {
     return arr.id == id;
   });
+  let [count, setCount] = useState(0);
+  let [alert, setAlert] = useState(true);
+  let [inputValue, setInputValue] = useState("");
 
+  useEffect(() => {
+    setTimeout(() => {
+      return isNaN(inputValue) ? window.alert("숫자를 입력해주세요") : null;
+    }, 0);
+  }, [inputValue]);
   return (
     <div className="container">
+      {count}
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        버튼
+      </button>
       <div className="row">
         <div className="col-md-6">
           <img
@@ -17,6 +33,13 @@ const Detail = (props) => {
           />
         </div>
         <div className="col-md-6">
+          <input
+            type="text"
+            placeholder="수량 입력란"
+            onChange={(e) => {
+              setInputValue(e.target.value);
+            }}
+          />
           <h4 className="pt-5">{findId.title}</h4>
           <p>{findId.content}</p>
           <p>{findId.price}won</p>
