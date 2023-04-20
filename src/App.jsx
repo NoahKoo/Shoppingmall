69f612cefdc2743470226cae2e055653ad5f00d7
Noah Koo <sjdpclsrnrk@gmail.com>
2023-04-20 12:16:45 +0900

    feat(App): Add routes, useNavigate, url Params

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,14 @@ import { useState } from "react";
 import "./App.css";
 import { Button, Col, Container, Nav, Navbar, Row } from "react-bootstrap";
 import data from "./data.js";
+import { Routes, Route, Link, useNavigate } from "react-router-dom";
+import Detail from "./routes/Detail";
+import About from "./routes/About";
+import Event from "./routes/Event";
 
 function App() {
   let [shoes] = useState(data);
+  let navigate = useNavigate();
 
   return (
     <div className="App">
@@ -12,33 +17,80 @@ function App() {
         <Container>
           <Navbar.Brand href="#home">NoahShop</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Cart</Nav.Link>
+            <Nav.Link
+              onClick={() => {
+                navigate("/");
+              }}
+            >
+              Home
+            </Nav.Link>
+            <Nav.Link
+              onClick={() => {
+                navigate("/detail");
+              }}
+            >
+              Detail
+            </Nav.Link>
+            <Nav.Link
+              onClick={() => {
+                navigate("/about");
+              }}
+            >
+              About
+            </Nav.Link>
+            <Nav.Link
+              onClick={() => {
+                navigate("/event");
+              }}
+            >
+              Event
+            </Nav.Link>
           </Nav>
         </Container>
       </Navbar>
-      <div className="main-bg"></div>
 
-      <Container>
-        <Row>
-          {shoes.map((a, i) => {
-            return (
-              <Col sm>
-                <Card shoes={shoes[i]} i={i} />
-              </Col>
-            );
-          })}
-          {/* <Col sm>
-            <Detail shoes={shoes[0]} i={1} />
-          </Col>
-          <Col sm>
-            <Detail shoes={shoes[1]} i={2} />
-          </Col>
-          <Col sm>
-            <Detail shoes={shoes[2]} i={3} />
-          </Col> */}
-        </Row>
-      </Container>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <div className="main-bg"></div>
+              <Container>
+                <Row>
+                  {shoes.map((a, i) => {
+                    return (
+                      <Col sm>
+                        <Card shoes={shoes[i]} i={i} />
+                      </Col>
+                    );
+                  })}
+                  {/* <Col sm>
+                      <Detail shoes={shoes[0]} i={1} />
+                    </Col>
+                    <Col sm>
+                      <Detail shoes={shoes[1]} i={2} />
+                    </Col>
+                    <Col sm>
+                      <Detail shoes={shoes[2]} i={3} />
+                    </Col> */}
+                </Row>
+              </Container>
+            </>
+          }
+        />
+        <Route path="/detail/:id" element={<Detail shoes={shoes} />} />
+
+        <Route path="/about" element={<About />}>
+          <Route path="member" element={<div>멤버임</div>} />
+          <Route path="location" element={<div>위치정보임</div>} />
+        </Route>
+        <Route path="/event" element={<Event />}>
+          <Route path="/event/one" element={<p>첫 주문시 양배추즙 서비스</p>} />
+          <Route path="/event/two" element={<p>생일기념 쿠폰받기</p>} />
+        </Route>
+
+        <Route path="*" element={<div>없는 페이지입니다.</div>} />
+      </Routes>
     </div>
   );
 }
