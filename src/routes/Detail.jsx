ecd91dbe8ab06af3b994126d29b9f12fdd37ef91
Noah Koo <sjdpclsrnrk@gmail.com>
2023-04-20 12:15:33 +0900

    feat(Detail): add useParams();

---
+++ src/routes/Detail.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+
+const Detail = (props) => {
+  let { id } = useParams();
+  let findId = props.shoes.find((arr) => {
+    return arr.id == id;
+  });
+
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-md-6">
+          <img
+            src="https://codingapple1.github.io/shop/shoes1.jpg"
+            width="100%"
+          />
+        </div>
+        <div className="col-md-6">
+          <h4 className="pt-5">{findId.title}</h4>
+          <p>{findId.content}</p>
+          <p>{findId.price}원</p>
+          <button className="btn btn-danger">주문하기</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Detail;
