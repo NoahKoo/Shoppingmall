76e30c971c4f6c007f1a320a0e49fd6406858b07
Noah Koo <sjdpclsrnrk@naver.com>
2023-04-22 18:31:48 +0900

    feat(cart) : Add Cart

--- src/routes/Cart.jsx
+++ src/routes/Cart.jsx
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { Table } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import { changeName, changeAge } from "../store/userSlice";
+import { plusCount } from "../store";
 
 const Cart = () => {
+  let state = useSelector((state) => {
+    return state;
+  });
+  let dispatch = useDispatch();
+
   return (
     <div>
+      <h6>
+        {state.user.name}님 {state.user.age}세의 장바구니
+      </h6>
+      <button
+        onClick={() => {
+          dispatch(changeAge(100));
+        }}
+      >
+        버튼
+      </button>
       <h4>장바구니</h4>
       <Table bordered hover>
         <thead>
@@ -15,12 +33,18 @@ const Cart = () => {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>1</td>
-            <td>안녕</td>
-            <td>안녕</td>
-            <td>안녕</td>
-          </tr>
+          {state.cart.map((arr, i) => {
+            return (
+              <tr key={i}>
+                <td>{state.cart[i].id}</td>
+                <td>{state.cart[i].name}</td>
+                <td>{state.cart[i].count}</td>
+                <td>
+                  <button onClick={() => {}}>+</button>
+                </td>
+              </tr>
+            );
+          })}
         </tbody>
       </Table>
     </div>
